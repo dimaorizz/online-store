3948d6d8c1f10a203428babe8e40b6c976868a40
Orizz <dimaorizz@gmail.com>
2020-01-20 12:11:24 +0200

    created passport-config

---
+++ passport-cfg.js
@@ -0,0 +1,30 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+
+const userTest = {
+    id: 1,
+    email: 'test@test.test',
+    password: '123',
+}
+
+passport.serializeUser((user, done) => {
+    console.log(`serialize: `, user);
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    console.log(`deserialize: `, id);
+    const user = userTest.id === id ? userTest : false;
+    done(null, user);
+})
+
+passport.use(
+    new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
+        if(email === userTest.email && password === userTest.password){
+            return done(null, user);
+        }
+        else{
+            return done(null, false);
+        }
+    })
+)
