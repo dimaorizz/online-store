fccde3275ebcc38781c556e17ffc3efcb768ee93
Orizz <dimaorizz@gmail.com>
2020-02-02 23:32:34 +0200

    added guest/user authorization

--- routes/mainPage.js
+++ routes/mainPage.js
@@ -8,7 +8,7 @@ const Goods = require('../models/Goods')
 // GET: localhost:3000/
 router.get('/' , async (req, res) => {
     const items = await Goods.find() // get all goods from database
-    res.render('mainPage', { items })
+    res.render('mainPage', { items, user: req.user !== undefined })
 })
 
 module.exports = router
